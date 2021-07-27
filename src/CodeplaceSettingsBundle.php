cd72e484750ee3fb22693d29e34e4cbb72ac0884
Maciej Żukiewicz <mac.zukiewicz@gmail.com>
2021-07-27 13:52:30 +0200

    initial commit

---
+++ src/CodeplaceSettingsBundle.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Codeplace\SettingsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class CodeplaceSettingsBundle extends Bundle
+{
+
+}
